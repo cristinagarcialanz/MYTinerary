e8515a16fc316d2855e7bf7cdf62e243fe95da5d
cristinagarcialanz <104571558+cristinagarcialanz@users.noreply.github.com>
2023-07-16 16:14:08 -0300

    Ajustando los itinerarios

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -7,7 +7,6 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -23,94 +22,90 @@ function Cities() {
 
   async function getData() {
     try {
-    citiesDB = await axios.get('https://cristina-api-cities-crud.onrender.com/api/cities');
-    const citiesData = citiesDB.data.response.cities;
+      citiesDB = await axios.get('https://cristina-api-cities-crud.onrender.com/api/cities');
+      const citiesData = citiesDB.data.response.cities;
       setCities(citiesData);
 
-  } catch (error) {
-    console.log(error);
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
 
   useEffect(() => {
     getData()
   }, [])
-const filteredCitiesByName = cities.filter((city) =>
-  city.name.toLowerCase().includes(searchTerm.toLowerCase())
-);
-const showNoResultsMessage = searchTerm !== '' && filteredCitiesByName.length === 0;
+  const filteredCitiesByName = cities.filter((city) =>
+    city.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const showNoResultsMessage = searchTerm !== '' && filteredCitiesByName.length === 0;
 
   return (
     <div className='containCities'>
       <SearchCities searchTerm={searchTerm} onSearchTermChange={setSearchTerm} />
 
-    <div className='in-construction'>
-      
-      {showNoResultsMessage ? (
+      <div className='in-construction'>
+
+        {showNoResultsMessage ? (
           <div className='messageNoFound'>No destinations found <img src="https://firebasestorage.googleapis.com/v0/b/mytinerary-cities.appspot.com/o/itineraries%2Fsearch%20(2).gif?alt=media&token=0a5ce9da-7e7a-4f14-ba62-58eedd311486" alt='' /></div>
         ) :
-      
-      filteredCitiesByName.length > 0 ?(
-        filteredCitiesByName.map((city) => (
-  <div key={city._id} className='cardCities'>
 
-              <Card sx={{ maxWidth: 345 }}>
-                <CardHeader
-                  avatar={
-                    <Avatar src={city.flag}>
+          filteredCitiesByName.length > 0 ? (
+            filteredCitiesByName.map((city) => (
+              <div key={city._id} className='cardCities'>
 
-                    </Avatar>
-                  }
-                  action={
-                    <IconButton aria-label="share" className='share'>
-                      <ShareIcon />
-                    </IconButton>
-                  }
-                  title={city.name}
-                  subheader={city.country}
-                />
-                <CardMedia
-                  component="img"
-                  height="194"
-                  image={city.image}
-                  alt="Ciudad"
-                />
-                <CardContent>
-                  <Typography variant="body2" color="text.secondary" padding="0.7rem" height="4rem">
-                    {city.description}
-                  </Typography>
-                </CardContent>
-                <div className='selects'>
-                  <CardActions disableSpacing>
-                  <IconButton aria-label="share" className='share'>
-                    <ShareIcon />
-                  </IconButton>
-                </CardActions>              
-                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    <LinkRouter to={'/CityDetail/' + city._id} className='explore'>                
-                <button className="explore-button">Explore More</button>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardHeader
+                    avatar={
+                      <Avatar src={city.flag}>
+
+                      </Avatar>
+                    }
+                    action={
+                      <IconButton aria-label="share" className='share'>
+                        <ShareIcon />
+                      </IconButton>
+                    }
+                    title={city.name}
+                    subheader={city.country}
+                  />
+                  <CardMedia
+                    component="img"
+                    height="194"
+                    image={city.image}
+                    alt="Ciudad"
+                  />
+                  <CardContent>
+                    <Typography variant="body2" color="text.secondary" padding="0.7rem" height="4rem">
+                      {city.description}
+                    </Typography>
+                  </CardContent>
+                  <div className='selects'>
+                    
+                    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                      <LinkRouter to={'/CityDetail/' + city._id} className='explore'>
+                        <button className="explore-button">Explore More</button>
                       </LinkRouter>
+                    </div>
+                  </div>
+                </Card>
+              </div>
+            )))
+            : (
+              <div>
+                <div className="core-page-loader splash">
+                  <div className="plane-spinner">
+                    <div className="spinner-plane">
+                    </div>
                   </div>
                 </div>
-              </Card>
-          </div>
-          )))
-        :(
-  <div>
-    <div className="core-page-loader splash">
-      <div className="plane-spinner">
-        <div className="spinner-plane">
-        </div>
-      </div>
-    </div>
-  </div>
+              </div>
 
-)}
-<a href=" " className="subir"><KeyboardArrowUpIcon /></a>
+            )}
+        <a href=" " className="subir"><KeyboardArrowUpIcon /></a>
+      </div >
     </div >
-    </div >
-    
-    
+
+
   );
 }
 export default Cities;
--- src/components/Itineraries.jsx
+++ src/components/Itineraries.jsx
@@ -31,31 +31,31 @@ const ExpandMore = styled((props) => {
 export default function Itineraries() {
 
   const [itineraries, setItinerary] = useState([]);
-  
+
   const { id } = useParams()
   const [favorites, setFavorites] = useState([]);
 
   async function getItinerary(cityID) {
     try {
-     
-         let itineraryDB
-        
-    itineraryDB = await axios.get('https://cristina-api-itineraries-crud.onrender.com/api/itineraries');
-    // {params:{cityID:cityID}}
-    console.log(itineraryDB.data.response);
-    const filteredItineraries = itineraryDB.data.response.itineraries.filter((itinerary) => itinerary.cityID === cityID);
-    setItinerary(filteredItineraries);
 
-    
-  } catch (error) {
-    console.error(error);
-  }
+      let itineraryDB
+
+      itineraryDB = await axios.get('https://cristina-api-itineraries-crud.onrender.com/api/itineraries');
+      // {params:{cityID:cityID}}
+      console.log(itineraryDB.data.response);
+      const filteredItineraries = itineraryDB.data.response.itineraries.filter((itinerary) => itinerary.cityID === cityID);
+      setItinerary(filteredItineraries);
+
+
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   useEffect(() => {
     getItinerary(id);
-    
-  },[id])
+
+  }, [id])
 
   const [expanded, setExpanded] = React.useState(false);
 
@@ -75,92 +75,93 @@ export default function Itineraries() {
 
   return (
     <>
-     {itineraries.length === 0 ? (
+      {itineraries.length === 0 ? (
 
-      <div className="messageNoFound">
-        <h3>Itinerary under construction...</h3>
-        <img src="https://firebasestorage.googleapis.com/v0/b/mytinerary-cities.appspot.com/o/itineraries%2Frecurring-settings.gif?alt=media&token=f0cf26e3-f677-4bf0-91ec-647c54841644" alt="" />
+        <div className="messageNoFound">
+          <h3>Itinerary under construction...</h3>
+          <img src="https://firebasestorage.googleapis.com/v0/b/mytinerary-cities.appspot.com/o/itineraries%2Frecurring-settings.gif?alt=media&token=f0cf26e3-f677-4bf0-91ec-647c54841644" alt="" />
         </div>
-     ) : ( 
-    
-    itineraries.map((itinerary) => (
-    <Card key={itinerary._id} className='cardItineraries' sx={{ minWidth: 345 }}>
-      <div className='viewPrincipal'>
-      <CardMedia
-      className='imageCardMedia'
-        component="img"
-        image="https://firebasestorage.googleapis.com/v0/b/mytinerary-cities.appspot.com/o/itineraries%2Fushuaia%2Fitinerary2%2Fcaption%20(2).jpg?alt=media&token=5ccd9ecd-b901-435b-871d-ba6663ea113a"
-        alt=""
-      />
-      <CardContent className='cardContentItinerary'>
-      <CardHeader
-        avatar={
-          <Avatar src={itinerary.imageAutor}>
-            </Avatar>
-        }
-        action={
-                  <IconButton
-          className={isFavorite(itinerary._id) ? 'favorite favorite-icon' : 'favorite'}
-          aria-label="add to favorites"
-          onClick={() => handleFavoriteClick(itinerary._id)}
-        >
-          <ThumbUpIcon /> 0
-        </IconButton>
-         }
-        title={itinerary.autor}
-      />
-      <Typography className='titleItineries'>{itinerary.itinerary}</Typography>
-        <Typography variant="body2" color="text.secondary">
-        {itinerary.description}
-        </Typography>
-         <CardActions disableSpacing className='iconsItinaries'>
-          <div className='iconsItinaries'>
-          <div> Price: {' '}
-          {Array(itinerary.price).fill().map((_, index) => (
-            <img key={index} src="https://firebasestorage.googleapis.com/v0/b/mytinerary-cities.appspot.com/o/itineraries%2Ficonos%2Fbillete-de-un-dolar.png?alt=media&token=4b6435c1-30f1-4c8e-a248-9e86235e7a85"
-              alt="dollar" width="35px"/>
-          ))} </div>   
-              
-        <div><img src="https://firebasestorage.googleapis.com/v0/b/mytinerary-cities.appspot.com/o/itineraries%2Ficonos%2Fzona-horaria.png?alt=media&token=6c65e432-30e1-4d7e-9f39-d450bb7b7d09" alt="dollar" width="35px"/>{itinerary.duration}</div>
-        <div className='hashtags'>
-          <ul>
-            {itinerary.hashtags.map((hashtag, index) => (
-              <li key={index}>{hashtag}</li>
-            ))}
-          </ul>
-        </div>    
-          
-        <div className='buttonExpand'>
-          <p>Explore More</p>
-           <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          
-          <ExpandMoreIcon />
-        </ExpandMore>
-        </div>
-        </div>
-       
-      </CardActions>
-      </CardContent>
-        </div>
-        <Collapse in={expanded} timeout="auto" unmountOnExit className='cardCarouselItinerary'>
-        <div className='titleItinerary'>
-                  <h2 className='itinerariesTitle'>Let's Explore</h2>
+      ) : (
 
-          </div>
-        <CardContent className='cardCarouselItineraries'>
+        itineraries.map((itinerary) => (
+          <Card key={itinerary._id} className='cardItineraries' sx={{ minWidth: 345 }}>
+            <div className='viewPrincipal'>
+              <CardMedia
+                className='imageCardMedia'
+                component="img"
+                image={itinerary.imageItinerary}
+                alt="imageItinerary"
+              />
+              <CardContent className='cardContentItinerary'>
+                <CardHeader
+                  avatar={
+                    <Avatar src={itinerary.imageAutor} style={{ width: '70px', height: '70px' }}>
+                      </Avatar>
+                  }
+                  action={
+                    <IconButton
+                      className={isFavorite(itinerary._id) ? 'favorite favorite-icon' : 'favorite'}
+                      aria-label="add to favorites"
+                      onClick={() => handleFavoriteClick(itinerary._id)}
+                    >
+                      <ThumbUpIcon /> 0
+                    </IconButton>
+                  }
+                  title={itinerary.autor}
+                  
+                />
+                <Typography className='titleItineries'>{itinerary.itinerary}</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {itinerary.description}
+                </Typography>
+                <CardActions disableSpacing className='iconsItinaries'>
+                  <div className='iconsItinaries'>
+                    <div> Price: {' '}
+                      {Array(itinerary.price).fill().map((_, index) => (
+                        <img key={index} src="https://firebasestorage.googleapis.com/v0/b/mytinerary-cities.appspot.com/o/itineraries%2Ficonos%2Fbillete-de-un-dolar.png?alt=media&token=4b6435c1-30f1-4c8e-a248-9e86235e7a85"
+                          alt="dollar" width="35px" />
+                      ))} </div>
 
-        <CarouselFlip images={itinerary.image} />
+                    <div><img src="https://firebasestorage.googleapis.com/v0/b/mytinerary-cities.appspot.com/o/itineraries%2Ficonos%2Fzona-horaria.png?alt=media&token=6c65e432-30e1-4d7e-9f39-d450bb7b7d09" alt="dollar" width="35px" />{itinerary.duration}</div>
+                    <div className='hashtags'>
+                      <ul>
+                        {itinerary.hashtags.map((hashtag, index) => (
+                          <li key={index}>{hashtag}</li>
+                        ))}
+                      </ul>
+                    </div>
 
-        </CardContent>
-      </Collapse>
-      
-    </Card>
-    )))}
+                    <div className='buttonExpand'>
+                      <p>Explore More</p>
+                      <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                      >
+
+                        <ExpandMoreIcon />
+                      </ExpandMore>
+                    </div>
+                  </div>
+
+                </CardActions>
+              </CardContent>
+            </div>
+            <Collapse in={expanded} timeout="auto" unmountOnExit className='cardCarouselItinerary'>
+              <div className='titleItinerary'>
+                <h2 className='itinerariesTitle'>Let's Explore</h2>
+
+              </div>
+              <CardContent className='cardCarouselItineraries'>
+
+                <CarouselFlip images={itinerary.image} />
+
+              </CardContent>
+            </Collapse>
+
+          </Card>
+        )))}
     </>
   );
 }
